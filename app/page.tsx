9862900ea398e2fd22386772ab2e3609d4ca8fb8
kajing1077 <kajing1077@gmail.com>
2025-04-14 17:04:54 +0900

    feat: Header 컴포넌트 추가 및 root page 더미 데이터 추가
    - 가게이름 등 더미데이터

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,32 @@
+// import MenuList from "./components/MenuList";
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <main>
+//         <div className="flex justify-between">
+//           <p>가게 이름</p>
+//           <p>기본홀 3번</p>
+//         </div>
+//         <div>네브바</div>
+//         <div>공지</div>
+//         <MenuList />
+//       </main>
+//     </div>
+//   );
+// }
 import MenuList from "./components/MenuList";
 
 export default function Home() {
   return (
     <div>
-      <main>
-        <div className="flex justify-between">
-          <p>가게 이름</p>
-          <p>기본홀 3번</p>
+      <main className="p-6">
+        <div className="flex justify-between items-center mb-4">
+          <p className="text-2xl font-semibold">포스트버거</p>
+          <p className="text-sm font-semibold text-slate-400">기본홀 3번</p>
         </div>
-        <div>네브바</div>
-        <div>공지</div>
+        {/* <div>공지</div> */}
+        <div>nav bar</div>
         <MenuList />
       </main>
     </div>
