3d1ba6eac5886a8ad55466f33cfe71ac8b16ba6c
kajing1077 <kajing1077@gmail.com>
2025-04-16 19:02:00 +0900

    style: UI 컴포넌트를 업데이트하여 일관성과 가독성을 개선 및 일부 UI 변경

--- app/components/NavItem.tsx
+++ app/components/NavItem.tsx
@@ -50,8 +50,12 @@ export default function NavItem({ label, href }: NavItemProps) {
   return (
     <Link href={href} onClick={handleClick}>
       <div
-        className={`flex-shrink-0 py-2 mr-6 font-semibold text-gray-700 
-       ${isActive ? "border-b-2 border-gray-700" : ""}`}
+        className={`flex-shrink-0 text-17 py-2 mr-6 font-semibold
+       ${
+         isActive
+           ? "border-b-2 border-b-tossgray-800 text-tossgray-800"
+           : "text-tossgray-600"
+       }`}
       >
         {label}
       </div>
