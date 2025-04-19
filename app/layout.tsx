5c1b707e42e4e48c49310861ce9ac33e6157214d
jykang24 <ttochi0105@naver.com>
2025-04-19 14:42:56 +0900

    feat: RootLayout에 Toast추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import MainHeader from "./components/MainHeader";
+import ToastPortal from "./components/ToastPortal";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
           <MainHeader />
         </header>
         {children}
+        <ToastPortal />
       </body>
     </html>
   );
