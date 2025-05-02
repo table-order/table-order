4e3ce4ed5776925717031fd1d33abf044a5ebe29
jykang24 <ttochi0105@naver.com>
2025-05-02 16:48:13 +0900

    feat: UserCount-접속자수 UI컴포넌트 추가

---
+++ app/components/UserCount.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useUserStore } from "../store/userStore";
+
+export default function UserCount() {
+  const users = useUserStore((state) => state.users);
+
+  return users.length > 1 ? (
+    <span className="text-17 text-tossgray-800 font-medium">
+      {users.length}명 함께 주문중
+    </span>
+  ) : (
+    <span className="text-17 text-tossgray-800 font-medium">
+      멤버도 QR 찍고 <span className="text-tossblue-500">함께 주문</span>해요
+    </span>
+  );
+}
